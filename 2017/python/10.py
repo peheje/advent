f523d9dad13f72244aae767a81016da9e66e0b6e
Peter Helstrup Jensen <peterhj@gmail.com>
2017-12-15 09:36:39 +0100

    part 10.2

--- 2017/python/10.py
+++ 2017/python/10.py
@@ -1,32 +1,57 @@
-def take_circular(x: list, pos: int, length: int):
+from functools import reduce
+
+
+def take_circular(x: iter, pos: int, length: int):
     c = [x[(pos + i) % len(x)] for i in range(length)]
     return c
 
 
-def insert_circular(main: list, sub: list, begin: int):
+def insert_circular(main: list, sub: iter, begin: int):
     for i, v in enumerate(sub):
         main[(begin + i) % len(main)] = v
     return main
 
 
-# knot = [i for i in range(5)]
-# lengths = [3, 4, 1, 5]
+def one_round(knot: list, pos: int, skip: int, lengths: iter):
+    for length in lengths:
+        sublist = take_circular(knot, pos, length)
+        knot = insert_circular(knot, reversed(sublist), pos)
+        pos += length + skip
+        skip += 1
+    return knot, pos, skip
 
-knot = [i for i in range(256)]
+
+def to_chunks(x: iter, n: int):
+    for i in range(0, len(x), n):
+        yield x[i:i + n]
+
+
+def xor_many(x: list, n: int):
+    xored_list = []
+    chunks = to_chunks(x, n)
+    for chunk in chunks:
+        xored = reduce(lambda a, b: a ^ b, chunk)
+        xored_list.append(xored)
+    return xored_list
+
+
 with open("../data/10.txt") as f:
-    lengths = [int(x) for x in f.readline().split(",")]
+    data = f.readline()
+lengths = [int(x) for x in data.split(",")]
+suffix = [17, 31, 73, 47, 23]
+lengths_ascii = [ord(c) for c in data] + suffix
 
-pos = 0
-skip = 0
+# Part 1
+knot = [i for i in range(256)]
+pos, skip = 0, 0
+knot, pos, skip = one_round(knot, pos, skip, lengths)
+print("pt1", knot[0] * knot[1])
 
-for length in lengths:
-    sublist = take_circular(knot, pos, length)
-    rev = list(reversed(sublist))
-    knot = insert_circular(knot, rev, pos)
-
-    pos += length + skip
-    skip += 1
-
-print(knot)
-
-print("pt1", knot[0]*knot[1])
+# Part 2
+pos, skip = 0, 0
+knot = [i for i in range(256)]
+for i in range(64):
+    knot, pos, skip = one_round(knot, pos, skip, lengths_ascii)
+dense = xor_many(knot, 16)
+knot_hash = "".join([format(x, '02x') for x in dense])
+print("pt2", knot_hash)
